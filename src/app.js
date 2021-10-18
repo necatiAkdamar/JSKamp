5cbd8feec24d315ade381049cd4efba40fd801c7
Necati AKDAMAR <necatiakdamar@hotmail.com>
2021-10-19 00:02:35 +0300

    18EkimDersSonu

---
+++ src/app.js
@@ -0,0 +1,21 @@
+console.log("Merhaba Kodlama.io")
+
+
+//dolarDun="Ankara"
+let dolarBugun=9.33
+let dolarDun=9.20
+{
+    let dolarDun=9.10
+}
+const euroDun=11
+console.log(dolarDun, euroDun)
+
+let konutKredileri=["konut kredisi","emlak konut kredisi","kamu konutları","işci konutları"]
+
+
+
+console.log("<ul>")
+for (let i = 0; i < konutKredileri.length; i++) {
+        console.log("<li>"+konutKredileri[i]+"</li>")
+}
+console.log("</ul>")
